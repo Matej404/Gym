99e38afc0a5d4f012122e368c586c63925edb656
Matej404 <matejjurin278@gmail.com>
2022-03-10 13:25:42 +0000

    features

--- src/web/Main.js
+++ src/web/Main.js
@@ -1,13 +1,17 @@
 import "./style.css";
 import Navbar from "./navbar/Navbar";
 import Home from "./home/Home";
+import Features from "./features/Features";
 
 export default function Main() {
 
     return(
         <div>
             <Navbar />
+            <div id="home" />
             <Home />
+            <div id="features" />
+            <Features />
         </div>
     )
 }
